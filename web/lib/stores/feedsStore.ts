9f7eaf2c07b7556db45864de561158dc913d1199
michaeltempesta <michaeltempesta@gmail.com>
2025-12-21 20:23:12 +0900

    fix: remove duplicate useMemo import in feedsStore.ts

--- web/lib/stores/feedsStore.ts
+++ web/lib/stores/feedsStore.ts
@@ -11,7 +11,6 @@
  */
 
 import { useMemo } from 'react';
-import { useMemo } from 'react';
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
